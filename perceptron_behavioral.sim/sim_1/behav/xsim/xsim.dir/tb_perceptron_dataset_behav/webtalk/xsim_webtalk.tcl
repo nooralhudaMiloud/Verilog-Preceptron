69a2a9b44a99a31acb8605d3351f5ce4770d9e0a
noorAlhudaMiloud <calsefer189@gmail.com>
2026-05-02 00:45:54 +0200

    final_edit

--- perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/xsim_webtalk.tcl
+++ perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri May  1 21:52:44 2026" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat May  2 00:41:57 2026" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b23e59ea-6f37-40ea-810b-ba74159e8505" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "9220e5f456894dda80417d8e4da5bc81" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "100" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "102" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "13th Gen Intel(R) Core(TM) i7-1355U" -context "user_environment"
@@ -27,7 +27,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "367915 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.17_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "11040_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3984776636 -regid "" -xml C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.11_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "10784_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 430517459 -regid "" -xml C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/HP/perceptron_behavioral/perceptron_behavioral/perceptron_behavioral.sim/sim_1/behav/xsim/xsim.dir/tb_perceptron_dataset_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
